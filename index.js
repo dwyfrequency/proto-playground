ca05a5fdff9c5e0a2c55a9cf90ed7abda3c8875c
dwyfrequency <jdwy215@gmail.com>
2021-05-30 11:40:34 -0400

    add some code

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const BarProtos = require('./build/gen/v3/bar_pb.js');
 
-console.log(proto);
+// console.log(BarProtos);
+
+const bar1 = new BarProtos.Bar();
+console.log(bar1);
+bar1.setCapacity(50);
+console.log(bar1.getCapacity());
